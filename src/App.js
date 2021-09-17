473a9b87fa66591512beff3d0fd0d35818e8d15a
CodingwithKinny <kinnard242@gmail.com>
2021-09-17 15:06:17 -0400

    'robo'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
+class App extends React.Component{
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -21,5 +23,5 @@ function App() {
     </div>
   );
 }
-
+}
 export default App;
